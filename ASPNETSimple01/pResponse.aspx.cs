8995402e08b5071284d4cdda81b3d197cdc67352
Romulo-Reis <romuloreistavates@hotmail.com>
2020-04-14 22:30:53 -0300

    Criação do formulário web pResponse.

---
+++ ASPNETSimple01/pResponse.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace ASPNETSimple01
+{
+    public partial class pRequest : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Response.Write("OK");
+
+            //Response.AddHeader("APPLICATION_INFORMATION","HELLO TEST");
+            //Response.Redirect("http://www.google.com");
+            Response.End();
+            Response.Write("OK2");
+        }
+    }
+}
